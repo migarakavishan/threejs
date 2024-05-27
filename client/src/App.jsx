1d90e15b226d694aadfe4279d17b977831b4b1a7
migarakavishan <migarakavishan43@gmail.com>
2024-05-27 20:25:57 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+import Canvas from "./canvas";
+
+function App() {
+  return (
+    <main>
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
